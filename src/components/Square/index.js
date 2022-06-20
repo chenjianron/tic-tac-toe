fd00ac489fce8921eb90d43d5046b3abf921aea7
陈坚镕 <2440398853@qq.com>
2022-06-20 10:48:03 +0800

    '搭建基础页面'

---
+++ src/components/Square/index.js
@@ -0,0 +1,8 @@
+import React, { Component } from "react";
+import "./index.css";
+
+export default class Square extends Component {
+  render() {
+    return <button className="square">{/* TODO */}</button>;
+  }
+}
